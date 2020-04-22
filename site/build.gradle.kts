75ea9bcba041780bf45bdcb0b984a9e460fa2a11
Casey Brooks <cjbrooks12@gmail.com>
2020-04-22 14:28:08 -0500

    Update main Orchid/site configuration

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -17,26 +17,13 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    //implementation(project(":theme"))
     orchidImplementation("io.github.javaeden.orchid:OrchidCore:${Versions.ORCHID}")
     orchidImplementation("io.github.javaeden.orchid:OrchidPosts:${Versions.ORCHID}")
-    //orchidImplementation("io.github.javaeden.orchid:OrchidBsDoc:${Versions.ORCHID}")
     orchidImplementation("io.github.javaeden.orchid:OrchidPages:${Versions.ORCHID}")
     orchidImplementation("io.github.javaeden.orchid:OrchidAsciidoc:${Versions.ORCHID}")
     orchidImplementation("io.github.javaeden.orchid:OrchidSourceDoc:${Versions.ORCHID}")
+    orchidImplementation("io.github.javaeden.orchid:OrchidGithub:${Versions.ORCHID}")
+    orchidImplementation("io.github.javaeden.orchid:OrchidSyntaxHighlighter:${Versions.ORCHID}")
+    orchidImplementation("io.github.javaeden.orchid:OrchidPluginDocs:${Versions.ORCHID}")
     testImplementation("junit", "junit", "4.12")
-}
-
-orchid {
-    theme = "MateoTheme"
-    baseUrl = "http://localhost:8080"
-
-    environment = if (findProperty("env") == "prod") { "prod" } else { "debug" }
-    //args = listOf("--experimentalSourceDoc")
-
-    githubToken = if (hasProperty("github_token")) {
-        property("github_token").toString()
-    } else {
-        System.getenv("GITHUB_TOKEN")
-    }
 }
--- site/src/orchid/kotlin/com/thanosfisherman/mateo/MateoTheme.kt
+++ site/src/orchid/kotlin/com/thanosfisherman/mateo/MateoTheme.kt
@@ -1,14 +1,10 @@
 package com.thanosfisherman.mateo
 
-import com.eden.common.util.EdenUtils
 import com.eden.orchid.api.OrchidContext
 import com.eden.orchid.api.options.annotations.*
 import com.eden.orchid.api.options.archetypes.ConfigArchetype
 import com.eden.orchid.api.theme.Theme
 import com.eden.orchid.api.theme.assets.CssPage
-import com.eden.orchid.api.theme.components.ComponentHolder
-import com.eden.orchid.api.theme.models.Social
-import org.json.JSONObject
 import javax.inject.Inject
 
 @Description("A theme based on the Bootstrap 3 documentation, and good for code documentation.", name = "Mateo")
@@ -17,55 +13,17 @@ class MateoTheme @Inject constructor(
     context: OrchidContext
 ) : Theme(context, "MateoTheme") {
 
-    @Option
-    @StringDefault("#4C376C")
-    @Description("The CSS HEX value for the site's primary color.")
-    lateinit var primaryColor: String
-
-    @Option
-    @StringDefault("#000000")
-    @Description("The CSS HEX value for the site's secondary color.")
-    lateinit var secondaryColor: String
-
-    @Option
-    @StringDefault("")
-    @Description("The CSS HEX value for the site's link color.")
-    lateinit var linkColor: String
-
-    @Option
-    @StringDefault("")
-    @Description("The CSS HEX value for the site's GitHub banner background color.")
-    lateinit var bannerColor: String
-
-    @Option
-    @Description("Your social media links.")
-    var social: Social? = null
-
-    @Option
-    @Description("Github project for 'Fork me on Github' ribbon")
-    lateinit var github: String
-
-    @Option
-    @Description("Custom options for Trianglify.")
-    lateinit var trianglifyOptions: JSONObject
-
-    @Option
-    @BooleanDefault(true)
-    @Description("Whether to show the current verision in the header and footer..")
-    var showVersion: Boolean = true
-
-    @Option
-    @Description("Components to include in the sidebar, below the page menu.")
-    lateinit var sidebar: ComponentHolder
-
     override fun loadAssets() {
         // these assets include relative references to font files, which become invalid if the asset it downloaded locally and so need to stay as external assets even in production
         addCss(
             CssPage(
                 this,
                 "theme",
-                context.getResourceEntry("https://netdna.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css", null),
-                "bootstrap.min",
+                context.getResourceEntry(
+                    "https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css",
+                    null
+                ),
+                "font-awesome.min",
                 null
             )
         )
@@ -74,35 +32,38 @@ class MateoTheme @Inject constructor(
                 this,
                 "theme",
                 context.getResourceEntry(
-                    "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+                    "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600",
                     null
                 ),
                 "font-awesome.min",
                 null
             )
         )
-        addCss("https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.0/gh-fork-ribbon.min.css")
-        addCss("assets/css/4-layouts/page.scss")
+        addCss(
+            CssPage(
+                this,
+                "theme",
+                context.getResourceEntry(
+                    "https://fonts.googleapis.com/css?family=Prata&display",
+                    null
+                ),
+                "font-awesome.min",
+                null
+            )
+        )
+        addCss("assets/css/main.scss")
 
-        addJs("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js")
-        addJs("https://netdna.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
-        addJs("https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.3.0/bootbox.min.js")
+        // add JS assets
+        addJs("assets/js/vendors/jquery-3.3.1.min.js")
+        addJs("assets/js/vendors/simple-jekyll-search.min.js")
+        addJs("assets/js/vendors/slick.min.js")
+
+        addJs("assets/js/vendors/instafeed.min.js")
+        addJs("assets/js/vendors/jquery.fitvids.js")
+        addJs("assets/js/vendors/masonry.pkgd.min.js")
+        addJs("assets/js/vendors/imagesloaded.pkgd.min.js")
+        addJs("assets/js/vendors/transition.js")
+        addJs("assets/js/vendors/zoom.min.js")
         addJs("assets/js/common.js")
-    }
-
-    override fun onPostExtraction() {
-        super.onPostExtraction()
-
-        if (EdenUtils.isEmpty(linkColor)) {
-            linkColor = primaryColor
-        }
-
-        if (EdenUtils.isEmpty(bannerColor)) {
-            bannerColor = secondaryColor
-        }
-    }
-
-    override fun getComponentHolders(): Array<ComponentHolder> {
-        return super.getComponentHolders() + arrayOf(sidebar)
     }
 }
